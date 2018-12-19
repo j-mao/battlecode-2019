7cfc3cd44a0ee251e42ce48fa6ff7e2b4217bdd9
j-mao <jerry@Jerrys-MacBook-Air.local>
2018-12-19 21:45:01 +1100

    Add untested Java template code
    It is not guaranteed that this code will compile as I don't have a
    compiler on my laptop. However it should cover the general gist of
    how Java works. Feel free to use this as a template for all bot
    implementation.

---
+++ learn_to_java/RobotPlayer.java
@@ -0,0 +1,83 @@
+/*
+ * Java crash-course for non-Java-speaking members of np-cgw
+ * If using this as a template please get rid of the useless comments!
+ */
+
+/*
+ * The package name must be identical to the directory name
+ */
+package learn_to_java;
+
+/*
+ * For easy code maintenance we split code for different types of robots
+ * into separate files
+ * So we must import our own code
+ */
+import learn_to_java.*;
+
+// IMPORTANT: When using as a template remember to change the names above!
+
+/*
+ * Import the Battlecode package
+ * Might be called something else, who knows
+ */
+import bc2019.*;
+
+/*
+ * Other useful packages to have
+ */
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+/*
+ * Name of class same as name of file
+ * public because why not
+ * strictfp to make sure floating point operations are portable and behave identically everywhere
+ */
+public strictfp class RobotPlayer {
+	/*
+	 * All super-important "global" variables go here
+	 * Robot-specific variables should go into their own file
+	 * Everything we have here should be static
+	 */
+	static RobotController rc;
+	static Random rng;
+
+	/*
+	 * The entry-point of this Battlecode player
+	 * The main function will most likely already exist in a "module" somewhere
+	 * The actual function name will depend on the specs though
+	 */
+	public static void run(RobotController rc) throws Exception {
+		// Initialise our global variables because NullPointerException's stuck
+		rng = new Random();
+		RobotPlayer.rc = rc;
+
+		while (true) {
+			// Do this for every turn
+			try {
+				// Call the relevant code
+				if (2+2 == 4) {
+					SpecificRobotController.runTurn();
+				} else
+					SpecificRobotController.runTurn();
+			} catch (Exception oh_no_i_messed_up) {
+				// Do nothing
+				// Better than crashing and losing instantly
+			} finally {
+				// Code to clean up, end our turn etc
+				rc.endTurn();
+			}
+		}
+	}
+
+	/*
+	 * All non-robot-specific library functions go here
+	 */
+	public static int squareAndAddNumbers(int a, int b) {
+		return a*a+b*b;
+	}
+}
+// This is not CPP
+// You do not need a semicolon after your class definition
---
+++ learn_to_java/SpecificRobotController.java
@@ -0,0 +1,68 @@
+/*
+ * Java crash-course for non-Java-speaking members of np-cgw
+ * If using this as a template please get rid of the useless comments!
+ */
+
+/*
+ * The package name must be identical to the directory name
+ */
+package learn_to_java;
+
+/*
+ * For easy code maintenance we split code for different types of robots
+ * into separate files
+ * So we must import our own common library
+ */
+import learn_to_java.*;
+
+// IMPORTANT: When using as a template remember to change the names above!
+
+/*
+ * Import the Battlecode package
+ * Might be called something else, who knows
+ */
+import bc2019.*;
+
+/*
+ * Other useful packages to have
+ */
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+/*
+ * Name of class same as name of file
+ * public because why not
+ * strictfp to make sure floating point operations are portable and behave identically everywhere
+ * Class hierarchy ensures that we can access all of our library functions directly
+ */
+public strictfp class SpecificRobotController extends RobotPlayer {
+	/*
+	 * All robot-specific variables go here
+	 * Everything we have here should be static
+	 */
+	static int numberOfButterfliesThatIHaveSeen;
+	// Note that ints are not objects and are auto-initialised to zero (not a null object)
+
+	/*
+	 * The function called by the main RobotPlayer to run a turn
+	 */
+	public static void runTurn() throws Exception {
+		if (2+2 == 4) {
+			// The "rc" used here is the "global" declared in RobotPlayer.java
+			// We can access this because of the class hierarchy
+			rc.admireAnotherButterfly();
+
+			numberOfButterfliesThatIHaveSeen++;
+		}
+	}
+
+	/*
+	 * All robot-specific functions go here
+	 */
+	private static void eatAButterfly(int butterflyId) {
+		rc.omNomNom(butterflyId);
+	}
+}
+// This is not CPP
+// You do not need a semicolon after your class definition
