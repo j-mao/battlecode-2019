2c16726350da83958c3ad48608ca591512a9ec64
Jerry <jerry.w.mao@gmail.com>
2019-01-28 11:37:43 +1100

    Revert to old circle formulas
    Reverts aebd19d3844e0ee9f081ef56326f6b6deccc363e
    The previous circle formula had gradient of magnitude greater than 1,
    meaning that units couldn't catch up to the shrinking circle

--- newstart/MyRobot.java
+++ newstart/MyRobot.java
@@ -2020,11 +2020,11 @@ public strictfp class MyRobot extends BCAbstractRobot {
 	private class CircleRobotController extends MobileRobotController {
 
 		/**
-		 * Rate decreases inverse-linearly from initSqueezeRate
-		 * to finSqueezeRate.
+		 * Rate decreases linearly from initialSqueezeRate
+		 * to finalSqueezeRate.
 		 */
-		private final double invInitSqueezeRate;
-		private final double invFinSqueezeRate;
+		private final double initialSqueezeRate;
+		private final double finalSqueezeRate;
 		private final double totalSqueezeRadius;
 		private final double finalRadius;
 		private final double squeezeConst;
@@ -2046,13 +2046,13 @@ public strictfp class MyRobot extends BCAbstractRobot {
 			}
 			clock = 0;
 
-			invInitSqueezeRate = 64. / boardSize;
-			invFinSqueezeRate = 1. / 8.;
+			initialSqueezeRate = boardSize / 64.;
+			finalSqueezeRate = 0.05;
 
 			finalRadius = 4.0;
 			totalSqueezeRadius = (int) (1.42 * boardSize) - finalRadius;
-			squeezeConst = Math.log(invFinSqueezeRate / invInitSqueezeRate) / totalSqueezeRadius;
-			numSqueezeRounds = (int) Math.floor((invFinSqueezeRate - invInitSqueezeRate) / squeezeConst);
+			squeezeConst = (finalSqueezeRate * finalSqueezeRate - initialSqueezeRate * initialSqueezeRate) / (2 * totalSqueezeRadius);
+			numSqueezeRounds = (int) Math.round(2 * totalSqueezeRadius / (initialSqueezeRate + finalSqueezeRate));
 
 			minSminDist = 0;
 		}
@@ -2137,7 +2137,7 @@ public strictfp class MyRobot extends BCAbstractRobot {
 		}
 
 		private double getCircleRadius() {
-			double rad = totalSqueezeRadius + finalRadius - Math.log(1 + squeezeConst * clock / invInitSqueezeRate) / squeezeConst;
+			double rad = totalSqueezeRadius + finalRadius - initialSqueezeRate * clock - 0.5 * squeezeConst * clock * clock;
 			rad = Math.max(rad, finalRadius);
 			if (clock > numSqueezeRounds) rad = finalRadius;
 
